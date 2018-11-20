f1cc1bf4c1b1b4114965957cc8d647854ac2a281
ShuzoTakahashi <shuzo.create@gmail.com>
2018-11-20 17:03:30 +0900

    [update]リファクタリング（動作未テスト）

--- main.py
+++ main.py
@@ -1,55 +1,13 @@
 # coding=utf-8
-from com_socket import *
+
 from servo import *
+from pwm import *
 
 
-def kinect_to_robo_zero():
-    host = '192.168.43.181'  # ドメイン名、もしくはIPアドレス。socket.gethostname()を代入するとドメイン名を調べてくれる。
-    port = 55555  # wellknownにぶつからない適当なポート番号。クライアント側とサーバー側でポート番号を合わせる
-    connection = SupportSocketClient(host, port, 4)
-
-    try:
-        servo = PwmServoConfigData().get_RS306MD()
-        pwm1 = SupportServoDriver(address=0x40, config_data=servo)
-        pwm2 = SupportServoDriver(address=0x41, config_data=servo)
-
-        while True:
-            data = connection.recv_raw()
-
-            if data == b'':
-                break
-
-            # data = data.split(':')
-            # channel = int(data[0])
-            # angle = int(data[1])
-
-            sub_cmd = data[0]  # int.from_bytes((data[0]), 'big')
-            channel = data[1]
-            sign_flg = data[2]  # ここは符号を表す
-
-            # FIXME: あれ？ここのdata[2]はdata[3]の間違いじゃない？
-            if sign_flg == 0:
-                angle = int(data[2])
-            elif sign_flg == 1:
-                angle = int(data[2]) * -1
-
-            print('[Receive]')
-            print('id : {0}'.format(channel))
-            print('rotate : {0}\n'.format(angle))
-
-            # FIXME: 0°に出力してもうまく出来ない。数値や計算は問題ない。+12°くらいで実際の0°になる。
-            angle += 12
-
-            # 16以下なら上半身
-            if channel < 16:
-                pwm1.to_angle(channel, angle)
-
-            else:
-                channel -= 16
-                pwm2.to_angle(channel, angle)
-    finally:
-        connection.close()
+def hexapod_forward():
+    servo = PwmServoConfigData().get_MG92B()
+    pwm = SupportServoDriver(config_data=servo)
 
 
 if __name__ == '__main__':
-    pwm_servo_driver_test(PwmServoConfigData().get_RS306MD())
+    hexapod_forward()
