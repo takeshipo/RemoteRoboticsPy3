447715f47fdfc82c0c852a806ed9d1b685807a88
shuzotakahashi <shuzo.create@gmail.com>
2018-12-05 09:57:19 +0900

    [change]通信テスト用

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 # coding=utf-8
-from hexapod import *
 from tests import *
 from servo import *
+from com_socket import *
 
 if __name__ == '__main__':
-    pwm_driver_test(ServoPwmConfigData().get_SG90())
+
+    host = "192.168.10.7"  # ドメイン名、もしくはIPアドレス。socket.gethostname()を代入するとドメイン名を調べてくれる。
+    port = 55555  # wellknownにぶつからない適当なポート番号。クライアント側とサーバー側でポート番号を合わせる
+    connection = SupportSocketServer(host, port)
+
+    try:
+
+        while True:
+            data = connection.recv_raw()
+
+            if data == b'a':
+                print("aaa")
+                break
+
+            elif data == b'b':
+                print("bbb")
+                break
+
+            elif data == b'q':
+                print("quit")
+                break
+
+    finally:
+        connection.close()
