598579adceb64375e9f5ffc0d2c25bc2ab3aa562
ShuzoTakahashi <shuzo.create@gmail.com>
2019-01-25 15:38:14 +0900

    refactor:あっきーのソースを修正

--- hexapod.py
+++ hexapod.py
@@ -4,13 +4,14 @@ from servo import *
 import time
 from robotics import Robotics
 
-#角度変数
-#左足の動作
-leftn=90   #左足の高さ初期値
-leftup=-60 #左脚の上げる高さ
-#右足の動作
-rightn=-90 #右脚高さの初期値
-rightup=60 #右脚の上げる高さ
+# 角度変数
+# 左足の動作
+leftn = 90  # 左足の高さ初期値
+leftup = -60  # 左脚の上げる高さ
+# 右足の動作
+rightn = -90  # 右脚高さの初期値
+rightup = 60  # 右脚の上げる高さ
+
 
 class Hexapod3axis(Robotics):
 
@@ -24,14 +25,14 @@ class Hexapod3axis(Robotics):
         print("前進")
 
     def on_backward(self):
-        print("後進")  
+        print("後進")
 
     def on_left_turn(self):
         print("左旋回")
-   
+
     def on_right_turn(self):
         print("右旋回")
-   
+
     def on_stop(self):
         print('ストップ')
 
@@ -47,358 +48,355 @@ class Hexapod2axis(Robotics):
 
     def on_forward(self):
         print("前進")
-        #---------------------------------------
+        # ---------------------------------------
         # ニュートラルポジション 0.05sec
-        #---------------------------------------
+        # ---------------------------------------
         '''
-        self.pwm.to_angle(1, leftn)
+        self.driver.to_angle(1, leftn)
         time.sleep(0.004)
-        self.pwm.to_angle(7, rightn)
+        self.driver.to_angle(7, rightn)
         time.sleep(0.004)
-        self.pwm.to_angle(9, leftn) 
+        self.driver.to_angle(9, leftn) 
         time.sleep(0.004)
-        self.pwm.to_angle(3, rightn)
+        self.driver.to_angle(3, rightn)
         time.sleep(0.004)
-        self.pwm.to_angle(5, leftn) 
+        self.driver.to_angle(5, leftn) 
         time.sleep(0.004)
-        self.pwm.to_angle(11, rightn)
-        time.sleep(0.004)
-
-        self.pwm.to_angle(0, 30)
-        time.sleep(0.004)
-        self.pwm.to_angle(6, 0) 
-        time.sleep(0.004)
-        self.pwm.to_angle(8, -30)
+        self.driver.to_angle(11, rightn)
         time.sleep(0.004)
 
-        self.pwm.to_angle(2, -30)
+        self.driver.to_angle(0, 30)
         time.sleep(0.004)
-        self.pwm.to_angle(4, 0)
+        self.driver.to_angle(6, 0) 
         time.sleep(0.004)
-        self.pwm.to_angle(10, 30)
+        self.driver.to_angle(8, -30)
+        time.sleep(0.004)
+
+        self.driver.to_angle(2, -30)
+        time.sleep(0.004)
+        self.driver.to_angle(4, 0)
+        time.sleep(0.004)
+        self.driver.to_angle(10, 30)
         time.sleep(0.004)
 
         time.sleep(0.05)
         '''
-        #---------------------------------------
+        # ---------------------------------------
         # 1段階動作
-        #---------------------------------------
-        self.pwm.to_angle(1, leftup)
+        # ---------------------------------------
+        self.driver.to_angle(1, leftup)
         time.sleep(0.004)
-        self.pwm.to_angle(7, rightup)
+        self.driver.to_angle(7, rightup)
         time.sleep(0.004)
-        self.pwm.to_angle(9, leftup)
+        self.driver.to_angle(9, leftup)
         time.sleep(0.004)
 
-        self.pwm.to_angle(3, rightn)
+        self.driver.to_angle(3, rightn)
         time.sleep(0.004)
-        self.pwm.to_angle(5, leftn)
+        self.driver.to_angle(5, leftn)
         time.sleep(0.004)
-        self.pwm.to_angle(11, rightn)
+        self.driver.to_angle(11, rightn)
         time.sleep(0.004)
-        
+
         time.sleep(0.1)
-        
-        #--------------------------------------
+
+        # --------------------------------------
         # 2段階動作
-        #--------------------------------------
+        # --------------------------------------
 
-        self.pwm.to_angle(0, 60)
+        self.driver.to_angle(0, 60)
         time.sleep(0.004)
-        self.pwm.to_angle(6, -30)
+        self.driver.to_angle(6, -30)
         time.sleep(0.004)
-        self.pwm.to_angle(8, 0)
+        self.driver.to_angle(8, 0)
         time.sleep(0.004)
 
-        self.pwm.to_angle(2, 0)
+        self.driver.to_angle(2, 0)
         time.sleep(0.004)
-        self.pwm.to_angle(4, -30)
+        self.driver.to_angle(4, -30)
         time.sleep(0.004)
-        self.pwm.to_angle(10, 60)
+        self.driver.to_angle(10, 60)
         time.sleep(0.004)
 
         time.sleep(0.1)
 
-        #--------------------------------------
+        # --------------------------------------
         # 3段階動作
-        #--------------------------------------
+        # --------------------------------------
 
-        self.pwm.to_angle(1, leftn)
+        self.driver.to_angle(1, leftn)
         time.sleep(0.004)
-        self.pwm.to_angle(7, rightn)
+        self.driver.to_angle(7, rightn)
         time.sleep(0.004)
-        self.pwm.to_angle(9, leftn) 
+        self.driver.to_angle(9, leftn)
         time.sleep(0.004)
 
-        self.pwm.to_angle(3, rightup)
+        self.driver.to_angle(3, rightup)
         time.sleep(0.004)
-        self.pwm.to_angle(5, leftup)
+        self.driver.to_angle(5, leftup)
         time.sleep(0.004)
-        self.pwm.to_angle(11, rightup)
+        self.driver.to_angle(11, rightup)
         time.sleep(0.004)
 
         time.sleep(0.1)
-                
-        #--------------------------------------
+
+        # --------------------------------------
         # 4段階動作
-        #--------------------------------------
-        self.pwm.to_angle(0, 0)
+        # --------------------------------------
+        self.driver.to_angle(0, 0)
         time.sleep(0.004)
-        self.pwm.to_angle(6, 30)
+        self.driver.to_angle(6, 30)
         time.sleep(0.004)
-        self.pwm.to_angle(8, -60)
+        self.driver.to_angle(8, -60)
         time.sleep(0.004)
 
-        self.pwm.to_angle(2, -60)
+        self.driver.to_angle(2, -60)
         time.sleep(0.004)
-        self.pwm.to_angle(4, 30)
+        self.driver.to_angle(4, 30)
         time.sleep(0.004)
-        self.pwm.to_angle(10, 0)
+        self.driver.to_angle(10, 0)
         time.sleep(0.004)
 
         time.sleep(0.1)
-
 
     def on_backward(self):
         print("後進")
-        #---------------------------------------
+        # ---------------------------------------
         # 1段階動作
-        #---------------------------------------
-        self.pwm.to_angle(1, leftup)
+        # ---------------------------------------
+        self.driver.to_angle(1, leftup)
         time.sleep(0.004)
-        self.pwm.to_angle(7, rightup)
+        self.driver.to_angle(7, rightup)
         time.sleep(0.004)
-        self.pwm.to_angle(9, leftup)
+        self.driver.to_angle(9, leftup)
         time.sleep(0.004)
 
-        self.pwm.to_angle(3, rightn)
+        self.driver.to_angle(3, rightn)
         time.sleep(0.004)
-        self.pwm.to_angle(5, leftn)
+        self.driver.to_angle(5, leftn)
         time.sleep(0.004)
-        self.pwm.to_angle(11, rightn)
+        self.driver.to_angle(11, rightn)
         time.sleep(0.004)
-        
+
         time.sleep(0.1)
-        
-        #--------------------------------------
+
+        # --------------------------------------
         # 2段階動作
-        #--------------------------------------
+        # --------------------------------------
 
-        self.pwm.to_angle(0, 0)
+        self.driver.to_angle(0, 0)
         time.sleep(0.004)
-        self.pwm.to_angle(6, 30)
+        self.driver.to_angle(6, 30)
         time.sleep(0.004)
-        self.pwm.to_angle(8, -60)
+        self.driver.to_angle(8, -60)
         time.sleep(0.004)
 
-        self.pwm.to_angle(2, -60)
+        self.driver.to_angle(2, -60)
         time.sleep(0.004)
-        self.pwm.to_angle(4, 30)
+        self.driver.to_angle(4, 30)
         time.sleep(0.004)
-        self.pwm.to_angle(10, 0)
+        self.driver.to_angle(10, 0)
         time.sleep(0.004)
 
         time.sleep(0.1)
 
-        #--------------------------------------
+        # --------------------------------------
         # 3段階動作
-        #--------------------------------------
+        # --------------------------------------
 
-        self.pwm.to_angle(1, leftn)
+        self.driver.to_angle(1, leftn)
         time.sleep(0.004)
-        self.pwm.to_angle(7, rightn)
+        self.driver.to_angle(7, rightn)
         time.sleep(0.004)
-        self.pwm.to_angle(9, leftn)
+        self.driver.to_angle(9, leftn)
         time.sleep(0.004)
 
-        self.pwm.to_angle(3, rightup)
+        self.driver.to_angle(3, rightup)
         time.sleep(0.004)
-        self.pwm.to_angle(5, leftup)
+        self.driver.to_angle(5, leftup)
         time.sleep(0.004)
-        self.pwm.to_angle(11, rightup)
+        self.driver.to_angle(11, rightup)
         time.sleep(0.004)
 
         time.sleep(0.1)
-                
-        #--------------------------------------
+
+        # --------------------------------------
         # 4段階動作
-        #--------------------------------------
-        self.pwm.to_angle(0, 60)
+        # --------------------------------------
+        self.driver.to_angle(0, 60)
         time.sleep(0.004)
-        self.pwm.to_angle(6, -30)
+        self.driver.to_angle(6, -30)
         time.sleep(0.004)
-        self.pwm.to_angle(8, 0)
+        self.driver.to_angle(8, 0)
         time.sleep(0.004)
 
-        self.pwm.to_angle(2, 0)
+        self.driver.to_angle(2, 0)
         time.sleep(0.004)
-        self.pwm.to_angle(4, -30)
+        self.driver.to_angle(4, -30)
         time.sleep(0.004)
-        self.pwm.to_angle(10, 60)
+        self.driver.to_angle(10, 60)
         time.sleep(0.004)
 
         time.sleep(0.1)
-
 
     def on_left_turn(self):
         print("左旋回")
-        #---------------------------------------
+        # ---------------------------------------
         # 1段階動作
-        #---------------------------------------
-        self.pwm.to_angle(1, leftup)
+        # ---------------------------------------
+        self.driver.to_angle(1, leftup)
         time.sleep(0.004)
-        self.pwm.to_angle(7, rightup)
+        self.driver.to_angle(7, rightup)
         time.sleep(0.004)
-        self.pwm.to_angle(9, leftup)
+        self.driver.to_angle(9, leftup)
         time.sleep(0.004)
 
-        self.pwm.to_angle(3, rightn)
+        self.driver.to_angle(3, rightn)
         time.sleep(0.004)
-        self.pwm.to_angle(5, leftn)
+        self.driver.to_angle(5, leftn)
         time.sleep(0.004)
-        self.pwm.to_angle(11, rightn)
+        self.driver.to_angle(11, rightn)
         time.sleep(0.004)
-        
+
         time.sleep(0.1)
-        
-        #--------------------------------------
+
+        # --------------------------------------
         # 2段階動作
-        #--------------------------------------
+        # --------------------------------------
 
-        self.pwm.to_angle(0, 0)
+        self.driver.to_angle(0, 0)
         time.sleep(0.004)
-        self.pwm.to_angle(6, -30)
+        self.driver.to_angle(6, -30)
         time.sleep(0.004)
-        self.pwm.to_angle(8, -60)
+        self.driver.to_angle(8, -60)
         time.sleep(0.004)
 
-        self.pwm.to_angle(2, 60)
+        self.driver.to_angle(2, 60)
         time.sleep(0.004)
-        self.pwm.to_angle(4, 30)
+        self.driver.to_angle(4, 30)
         time.sleep(0.004)
-        self.pwm.to_angle(10, 0)
+        self.driver.to_angle(10, 0)
         time.sleep(0.004)
 
         time.sleep(0.1)
 
-        #--------------------------------------
+        # --------------------------------------
         # 3段階動作
-        #--------------------------------------
+        # --------------------------------------
 
-        self.pwm.to_angle(1, leftn)
+        self.driver.to_angle(1, leftn)
         time.sleep(0.004)
-        self.pwm.to_angle(7, rightn)
+        self.driver.to_angle(7, rightn)
         time.sleep(0.004)
-        self.pwm.to_angle(9, leftn)
+        self.driver.to_angle(9, leftn)
         time.sleep(0.004)
 
-        self.pwm.to_angle(3, rightup)
+        self.driver.to_angle(3, rightup)
         time.sleep(0.004)
-        self.pwm.to_angle(5, leftup)
+        self.driver.to_angle(5, leftup)
         time.sleep(0.004)
-        self.pwm.to_angle(11, rightup)
+        self.driver.to_angle(11, rightup)
         time.sleep(0.004)
 
         time.sleep(0.1)
-                
-        #--------------------------------------
+
+        # --------------------------------------
         # 4段階動作
-        #--------------------------------------
-        self.pwm.to_angle(0, 60)
+        # --------------------------------------
+        self.driver.to_angle(0, 60)
         time.sleep(0.004)
-        self.pwm.to_angle(6, 30)
+        self.driver.to_angle(6, 30)
         time.sleep(0.004)
-        self.pwm.to_angle(8, 0)
+        self.driver.to_angle(8, 0)
         time.sleep(0.004)
 
-        self.pwm.to_angle(2, 0)
+        self.driver.to_angle(2, 0)
         time.sleep(0.004)
-        self.pwm.to_angle(4, -30)
+        self.driver.to_angle(4, -30)
         time.sleep(0.004)
-        self.pwm.to_angle(10, -60)
+        self.driver.to_angle(10, -60)
         time.sleep(0.004)
 
         time.sleep(0.1)
 
     def on_right_turn(self):
         print("右旋回")
-                #---------------------------------------
+        # ---------------------------------------
         # 1段階動作
-        #---------------------------------------
-        self.pwm.to_angle(1, leftup)
+        # ---------------------------------------
+        self.driver.to_angle(1, leftup)
         time.sleep(0.004)
-        self.pwm.to_angle(7, rightup)
+        self.driver.to_angle(7, rightup)
         time.sleep(0.004)
-        self.pwm.to_angle(9, leftup)
+        self.driver.to_angle(9, leftup)
         time.sleep(0.004)
 
-        self.pwm.to_angle(3, rightn)
+        self.driver.to_angle(3, rightn)
         time.sleep(0.004)
-        self.pwm.to_angle(5, leftn)
+        self.driver.to_angle(5, leftn)
         time.sleep(0.004)
-        self.pwm.to_angle(11, rightn)
+        self.driver.to_angle(11, rightn)
         time.sleep(0.004)
-        
+
         time.sleep(0.1)
-        
-        #--------------------------------------
+
+        # --------------------------------------
         # 2段階動作
-        #--------------------------------------
-        self.pwm.to_angle(0, 60)
+        # --------------------------------------
+        self.driver.to_angle(0, 60)
         time.sleep(0.004)
-        self.pwm.to_angle(6, 30)
+        self.driver.to_angle(6, 30)
         time.sleep(0.004)
-        self.pwm.to_angle(8, 0)
+        self.driver.to_angle(8, 0)
         time.sleep(0.004)
 
-        self.pwm.to_angle(2, 0)
+        self.driver.to_angle(2, 0)
         time.sleep(0.004)
-        self.pwm.to_angle(4, -30)
+        self.driver.to_angle(4, -30)
         time.sleep(0.004)
-        self.pwm.to_angle(10, -60)
+        self.driver.to_angle(10, -60)
         time.sleep(0.004)
 
         time.sleep(0.1)
- 
-        #--------------------------------------
+
+        # --------------------------------------
         # 3段階動作
-        #--------------------------------------
+        # --------------------------------------
 
-        self.pwm.to_angle(1, leftn)
+        self.driver.to_angle(1, leftn)
         time.sleep(0.004)
-        self.pwm.to_angle(7, rightn)
+        self.driver.to_angle(7, rightn)
         time.sleep(0.004)
-        self.pwm.to_angle(9, leftn)
+        self.driver.to_angle(9, leftn)
         time.sleep(0.004)
 
-        self.pwm.to_angle(3, rightup)
+        self.driver.to_angle(3, rightup)
         time.sleep(0.004)
-        self.pwm.to_angle(5, leftup)
+        self.driver.to_angle(5, leftup)
         time.sleep(0.004)
-        self.pwm.to_angle(11, rightup)
+        self.driver.to_angle(11, rightup)
         time.sleep(0.004)
 
         time.sleep(0.1)
-                
-        #--------------------------------------
+
+        # --------------------------------------
         # 4段階動作
-        #--------------------------------------
-        self.pwm.to_angle(0, 0)
+        # --------------------------------------
+        self.driver.to_angle(0, 0)
         time.sleep(0.004)
-        self.pwm.to_angle(6, -30)
+        self.driver.to_angle(6, -30)
         time.sleep(0.004)
-        self.pwm.to_angle(8, -60)
-        time.sleep(0.004)
-
-        self.pwm.to_angle(2, 60)
-        time.sleep(0.004)
-        self.pwm.to_angle(4, 30)
-        time.sleep(0.004)
-        self.pwm.to_angle(10, 0)
+        self.driver.to_angle(8, -60)
         time.sleep(0.004)
 
-        time.sleep(0.1) 
+        self.driver.to_angle(2, 60)
+        time.sleep(0.004)
+        self.driver.to_angle(4, 30)
+        time.sleep(0.004)
+        self.driver.to_angle(10, 0)
+        time.sleep(0.004)
 
+        time.sleep(0.1)
 
     def on_stop(self):
         print('ストップ')
@@ -406,32 +404,32 @@ class Hexapod2axis(Robotics):
 
     def on_neutral(self):
         print('ニュートラルポジション')
-        self.pwm.to_angle(1, leftn)
+        self.driver.to_angle(1, leftn)
         time.sleep(0.004)
-        self.pwm.to_angle(7, rightn)
+        self.driver.to_angle(7, rightn)
         time.sleep(0.004)
-        self.pwm.to_angle(9, leftn)
-        time.sleep(0.004)
-
-        self.pwm.to_angle(3, rightn)
-        time.sleep(0.004)
-        self.pwm.to_angle(5, leftn)
-        time.sleep(0.004)
-        self.pwm.to_angle(11, rightn)
+        self.driver.to_angle(9, leftn)
         time.sleep(0.004)
 
-        self.pwm.to_angle(0, 30)
+        self.driver.to_angle(3, rightn)
         time.sleep(0.004)
-        self.pwm.to_angle(6, 0)
+        self.driver.to_angle(5, leftn)
         time.sleep(0.004)
-        self.pwm.to_angle(8, -30)
+        self.driver.to_angle(11, rightn)
         time.sleep(0.004)
 
-        self.pwm.to_angle(2, -30)
+        self.driver.to_angle(0, 30)
         time.sleep(0.004)
-        self.pwm.to_angle(4, 0)
+        self.driver.to_angle(6, 0)
         time.sleep(0.004)
-        self.pwm.to_angle(10, 30)
+        self.driver.to_angle(8, -30)
+        time.sleep(0.004)
+
+        self.driver.to_angle(2, -30)
+        time.sleep(0.004)
+        self.driver.to_angle(4, 0)
+        time.sleep(0.004)
+        self.driver.to_angle(10, 30)
         time.sleep(0.004)
 
         time.sleep(0.2)
